c30e5effd38fa6c4028db82d2403ac915debf892
Samuel Arguello <sam.rene12@gmail.com>
2022-07-14 16:18:49 -0500

    feat: added text input that will log users search results

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
+import React, { useState } from "react";
 import { searchFunctionality } from "../src/Services/SearchResults";
 
 function App() {
-  searchFunctionality("jeans", 1);
+  // searchFunctionality("jeans", 1);
+  const [search, setSearch] = useState("");
 
-  return <div className="App"></div>;
+  const searchChangeHandler = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchHandler = (event) => {
+    event.preventDefault();
+    searchFunctionality(search, 1);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={searchHandler}>
+        <label>Search:</label>
+        <input onKeyUp={searchChangeHandler}></input>
+        <button>Go</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
