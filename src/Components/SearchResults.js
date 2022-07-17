59efe2a922802c2f087782ba0502534794f3fe7e
Samuel Arguello <sam.rene12@gmail.com>
2022-07-17 16:36:59 -0500

    feat: added create card component and displayed information

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -1,37 +1,37 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState } from "react";
 import { searchFunctionality } from "../Services/SearchFunctionality";
+import ResultsCards from "./ResultCards";
 
 const SearchResults = () => {
   const [results, setResults] = useState([]);
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    fetch(
-      `http://api.searchspring.net/api/search/search.json?siteId=scmq7n&q=${search}&resultsFormat=native&page=${page}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.results);
-        setResults(data.results);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [search, page]);
-
   const searchChangeHandler = (event) => {
     setSearch(event.target.value);
   };
 
-  const searchHandler = (event) => {
+  const searchHandler = async (event) => {
     event.preventDefault();
+    setPage(1);
+    let response = await searchFunctionality(search, page);
+    setResults(response);
   };
 
-  const paginationHandler = (event) => {
+  const forwadPageHandler = async (event) => {
     event.preventDefault();
     let pageNum = page + 1;
     setPage(pageNum);
+    let response = await searchFunctionality(search, page);
+    setResults(response);
+  };
+
+  const previousPageHandler = async (event) => {
+    event.preventDefault();
+    let pageNum = -1;
+    setPage(pageNum);
+    let response = await searchFunctionality(search, page);
+    setResults(response);
   };
 
   return (
@@ -43,14 +43,15 @@ const SearchResults = () => {
       </form>
       <div>
         {results.map((result, index) => {
-          return <p key={index}>{result.id}</p>;
+          //   return <p key={index}>{result.id}</p>;
+          return <ResultsCards key={index} {...result}></ResultsCards>;
         })}
       </div>
       <div>
-        <button onClick={paginationHandler}>forward</button>
+        <button onClick={previousPageHandler}>back</button>
+        <button onClick={forwadPageHandler}>forward</button>
       </div>
     </div>
   );
 };
-
 export default SearchResults;
